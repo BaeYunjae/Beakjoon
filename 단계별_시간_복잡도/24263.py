7d62fbca1707cf49f9da262101b12a75ede9d335
BaeYunjae <88019800+BaeYunjae@users.noreply.github.com>
2023-05-26 09:44:34 +0900

    Create 24263.py

---
+++ 단계별_시간_복잡도/24263.py
@@ -0,0 +1,12 @@
+# 알고리즘 수업 - 알고리즘의 수행 시간 2
+# MenOfPassion 알고리즘
+# MenOfPassion(A[], n) {
+#   sum <- 0;
+#   for i <- 1 to n
+#        sum <- sum + A[i]; # 코드1
+#    return sum;
+#}
+# 첫째 줄: 코드 1의 수행 횟수
+# 둘째 줄: 코드 1의 수행 횟수를 다항식으로 나타내었을 때, 최고차항의 차수
+print(int(input()))
+print(1)
