9454fdee3a675c762d9a6b23228e33c86a0f4c6b
BaeYunjae <88019800+BaeYunjae@users.noreply.github.com>
2023-07-04 15:35:45 +0900

    Update *2108.py

--- 단계별_심화2/*2108.py
+++ 단계별_심화2/*2108.py
@@ -8,14 +8,16 @@ n = int(input())
 num_list = [int(input()) for _ in range(n)]
 num_list.sort()
 
-num_cnt = Counter(num_list).most_common()
+num_cnt = Counter(num_list).most_common()  # 최빈값 리스트
 
-print(round(sum(num_list)/n))
+# 산술평균
+print(round(sum(num_list)/n))  
+# 중앙값
 print(num_list[n//2])
-
+# 최빈값 (2개 이상이면 2번째로 큰값)
 if len(num_cnt) > 1 and num_cnt[0][1] == num_cnt[1][1]:
     print(num_cnt[1][0])
 else:
     print(num_cnt[0][0])
-
+# 범위
 print(num_list[-1] - num_list[0])
