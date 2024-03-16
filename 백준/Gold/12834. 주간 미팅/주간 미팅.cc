c510ad192f1ac8145fa06e513998ce16506fb400
BaeYunjae <88019800+BaeYunjae@users.noreply.github.com>
2024-03-16 16:04:43 +0900

    [Gold IV] Title: 주간 미팅, Time: 32 ms, Memory: 2324 KB -BaekjoonHub

---
+++ 백준/Gold/12834. 주간 미팅/주간 미팅.cc
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+struct Node {
+	int num, cost;
+
+	bool operator<(Node right) const {
+		return cost > right.cost;
+	}
+};
+
+int n, v, e;
+int a, b;
+int mems[101];
+vector<Node> vect[1001];
+
+int ans = 0;
+
+int dijkstra(int num) {
+	priority_queue<Node> pq;
+
+	int dist[1001];
+	for (int i = 1; i <= v; i++) dist[i] = 1e9;
+	dist[num] = 0;
+
+	pq.push({ num, 0 });
+
+	while (!pq.empty()) {
+		Node now = pq.top();
+		pq.pop();
+
+		for (int i = 0; i < vect[now.num].size(); i++) {
+			Node next = vect[now.num][i];
+
+			int nextCost = now.cost + next.cost;
+			if (dist[next.num] <= nextCost) continue;
+
+			dist[next.num] = nextCost;
+			pq.push({ next.num, nextCost });
+		}
+	}
+	
+	int toKIST = dist[a];
+	int toFood = dist[b];
+
+	if (toKIST == 1e9 || toFood == 1e9) ans = -1;
+	else ans += toKIST + toFood;
+
+	return ans;
+}
+
+int main() {
+	cin >> n >> v >> e >> a >> b;
+
+	for (int i = 0; i < n; i++) {
+		cin >> mems[i];
+	}
+
+	for (int i = 0; i < e; i++) {
+		int from, to, cost;
+		cin >> from >> to >> cost;
+
+		vect[from].push_back({ to, cost });
+		vect[to].push_back({ from, cost });
+	}
+
+	for (int i = 0; i < n; i++) {
+		dijkstra(mems[i]);
+	}
+
+	cout << ans;
+}
