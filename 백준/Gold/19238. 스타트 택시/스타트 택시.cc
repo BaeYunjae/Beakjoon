c22f9e5645dc505f308f9b710a7947323d6b8dcc
BaeYunjae <88019800+BaeYunjae@users.noreply.github.com>
2024-04-01 21:35:26 +0900

    [Gold II] Title: 스타트 택시, Time: 4 ms, Memory: 2852 KB -BaekjoonHub

--- 백준/Gold/19238. 스타트 택시/스타트 택시.cc
+++ 백준/Gold/19238. 스타트 택시/스타트 택시.cc
@@ -7,36 +7,35 @@ using namespace std;
 
 struct User {
 	int startY, startX, destY, destX;
-	int distFromTaxi = -1, distFromDest;
-
-	bool operator<(User right) const {
-		if (distFromTaxi == right.distFromTaxi) {
-			if (startY == right.startY)
-				return startX > right.startX;
-			return startY > right.startY;
-		}
-
-		return distFromTaxi > right.distFromTaxi;
-	}
+	bool flag = false;
+	int distToDest;
 };
 
 struct Taxi {
-	int nowY, nowX, fuel, startFuel, destFuel;
+	int nowY, nowX, fuel;
 };
+
+bool compare(User const a, User const b) {
+	if (a.distToDest == b.distToDest) {
+		if (a.startY == b.startY) {
+			return a.startX < b.startX;
+		}
+		return a.startY < b.startY;
+	}
+	return a.distToDest < b.distToDest;
+}
 
 int n, m, initFuel;
 int map[21][21];
-int visited[21][21];
 Taxi taxi;
-vector<User> initUsers;
-priority_queue<User> users;
+vector<User> users;
+int visited[21][21];
+int userTaxiMap[402][21][21];
 
 int dir[4][2] = { -1, 0, 1, 0, 0, -1, 0, 1 };
 
 void input() {
 	cin >> n >> m >> initFuel;
-
-	taxi.fuel = initFuel;
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
@@ -44,26 +43,25 @@ void input() {
 		}
 	}
 
-	// 택시 초기 위치
-	cin >> taxi.nowY >> taxi.nowX;
-	taxi.nowY--;
-	taxi.nowX--;
+	taxi.fuel = initFuel;
 
-	// 처음 승객들 정보 저장
+	int y, x;
+	cin >> y >> x;
+
+	taxi.nowY = y - 1;
+	taxi.nowX = x - 1;
+
 	for (int i = 0; i < m; i++) {
 		int sy, sx, dy, dx;
 		cin >> sy >> sx >> dy >> dx;
-
-		initUsers.push_back({ sy - 1, sx - 1, dy - 1, dx - 1 });
+		users.push_back({ sy - 1, sx - 1, dy - 1, dx - 1, false });
 	}
 }
 
-void findDist(int sy, int sx) {
-	memset(visited, 0, sizeof(visited));
-
+void initUserMap(int num, int sy, int sx) {
 	queue<pair<int, int>> q;
 	q.push({ sy, sx });
-	visited[sy][sx] = 1;
+	userTaxiMap[num][sy][sx] = 1;
 
 	while (!q.empty()) {
 		int nowY = q.front().first;
@@ -73,112 +71,98 @@ void findDist(int sy, int sx) {
 		for (int i = 0; i < 4; i++) {
 			int ny = nowY + dir[i][0];
 			int nx = nowX + dir[i][1];
+
 			if (ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
 			if (map[ny][nx] == 1) continue;
-			if (visited[ny][nx] != 0) continue;
+			if (userTaxiMap[num][ny][nx] != 0) continue;
 
-			visited[ny][nx] = visited[nowY][nowX] + 1;
-			q.push({ ny, nx });
-		}
-	}
-}
-
-void userToDest(int num, int sy, int sx) {
-	memset(visited, 0, sizeof(visited));
-
-	queue<pair<int, int>> q;
-	q.push({ sy, sx });
-	visited[sy][sx] = 1;
-
-	while (!q.empty()) {
-		int nowY = q.front().first;
-		int nowX = q.front().second;
-		q.pop();
-
-		for (int i = 0; i < 4; i++) {
-			int ny = nowY + dir[i][0];
-			int nx = nowX + dir[i][1];
-			if (ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
-			if (map[ny][nx] == 1) continue;
-			if (visited[ny][nx] != 0) continue;
-
-			visited[ny][nx] = visited[nowY][nowX] + 1;
-			if (ny == initUsers[num].destY && nx == initUsers[num].destX) {
-				initUsers[num].distFromDest = visited[ny][nx] - 1;
-				return;
-			}
+			userTaxiMap[num][ny][nx] = userTaxiMap[num][nowY][nowX] + 1;
 			q.push({ ny, nx });
 		}
 	}
 }
 
 int main() {
-
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 	cout.tie(0);
 
 	input();
-	findDist(taxi.nowY, taxi.nowX);
 
-	// 처음 택시와 승객들과의 거리
 	for (int i = 0; i < m; i++) {
-		initUsers[i].distFromTaxi = visited[initUsers[i].startY][initUsers[i].startX] - 1;
+		User now = users[i];
+		initUserMap(i, now.startY, now.startX);
 	}
 
-	// 각 승객들 도착점까지의 거리
+	bool flag = true;
 	for (int i = 0; i < m; i++) {
-		if (initUsers[i].distFromTaxi == -1) continue;
-		userToDest(i, initUsers[i].startY, initUsers[i].startX);
+
+		// 택시가 승객 1명에게라도 못가면 끝
+		if (userTaxiMap[i][taxi.nowY][taxi.nowX] == 0) {
+			flag = false;
+			break;
+		}
+
+		// 승객을 도착지까지 못가면 끝
+		if (userTaxiMap[i][users[i].destY][users[i].destX] == 0) {
+			flag = false;
+			break;
+		}
+	}
+	if (!flag) {
+		cout << -1;
+		return 0;
 	}
 
+	int used = 0;
+	while (used != m) {
 
-	for (int i = 0; i < m; i++) {
-		if (initUsers[i].distFromDest == 0) {
+		int minDist = 1e9;
+		int minUser = 0;
+
+		for (int i = 0; i < m; i++) {
+			int nowDist = userTaxiMap[i][taxi.nowY][taxi.nowX] - 1;
+
+			if (users[i].flag) continue;
+
+			if (minDist > nowDist) {
+				minDist = nowDist;
+				minUser = i;
+			}
+			else if (minDist == nowDist) {
+				if (users[i].startY == users[minUser].startY) {
+					if (users[i].startX < users[minUser].startX) {
+						minUser = i;
+					}
+				}
+				else if (users[i].startY < users[minUser].startY) {
+					minUser = i;
+				}
+			}
+		}
+
+		used++;
+		users[minUser].flag = true;
+		int taxiToUser = userTaxiMap[minUser][taxi.nowY][taxi.nowX] - 1;
+		int userToDest = userTaxiMap[minUser][users[minUser].destY][users[minUser].destX] - 1;
+
+		if (taxi.fuel < taxiToUser + userToDest) {
 			cout << -1;
 			return 0;
 		}
-		users.push(initUsers[i]);
+
+		taxi.fuel = taxi.fuel - taxiToUser - userToDest + 2 * userToDest;
+
+		taxi.nowY = users[minUser].destY;
+		taxi.nowX = users[minUser].destX;
 	}
 
-
-	while (!users.empty()) {
-		User nowUser = users.top();
-		users.pop();
-
-		taxi.startFuel = nowUser.distFromTaxi;
-		taxi.destFuel = nowUser.distFromDest;
-
-		if (taxi.startFuel + taxi.destFuel > taxi.fuel) {
+	for (int i = 0; i < m; i++) {
+		if (!users[i].flag) {
 			cout << -1;
 			return 0;
-		}
-		else {
-			taxi.fuel = taxi.fuel - taxi.startFuel - taxi.destFuel + taxi.destFuel * 2;
-			taxi.nowY = nowUser.destY;
-			taxi.nowX = nowUser.destX;
-
-			findDist(taxi.nowY, taxi.nowX);
-
-			queue<User> temp;
-			while (!users.empty()) {
-				User remainUser = users.top();
-				users.pop();
-
-				remainUser.distFromTaxi = visited[remainUser.startY][remainUser.startX] - 1;
-				temp.push(remainUser);
-			}
-
-			while (!temp.empty()) {
-				User remainUser = temp.front();
-				temp.pop();
-
-				users.push(remainUser);
-			}
 		}
 	}
 
 	cout << taxi.fuel;
-
-	return 0;
 }
