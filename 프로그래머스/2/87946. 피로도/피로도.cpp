d64807918023682cb43bb8027b1880bbf512bae9
BaeYunjae <88019800+BaeYunjae@users.noreply.github.com>
2024-04-12 23:51:47 +0900

    [level 2] Title: 피로도, Time: 0.04 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -3,47 +3,30 @@
 
 using namespace std;
 
+int n;
 int answer = -1;
-int dunNum;
-int used[9];
-int path[9];
 
-void dfs(int level, int hp, vector<vector<int>> duns){
-    if (level == dunNum){
-        int now = hp;
-        int cnt = 0;
-        for (int i = 0; i < dunNum; i++){
-            // 현재 피로도가 던전 최소 필요 이상이면 고
-            if (now >= duns[path[i]][0]){
-                cnt++;
-                now -= duns[path[i]][1];
-            }
-            // 아니면 넘어가기
-            // 만약 현재 cnt + 남은 던전 수가 현재 최댓값과 같거나 작으면
-            // 넘어가기
-            if ((cnt + (dunNum - 1 - i)) <= answer){
-                return;
-            }
-        }
-        
-        if (answer < cnt) answer = cnt;
-       
-        return;
+int visited[9];
+
+void dfs(int level, int hp, vector<vector<int>> dungeons){
+    if (answer < level){
+        answer = level;
     }
     
-    for (int i = 0; i < dunNum; i++){
-        if (used[i] == 1) continue;
+    for (int i = 0; i < n; i++){
+        if (visited[i] || dungeons[i][0] > hp) continue;
         
-        path[level] = i;
-        used[i] = 1;
-        dfs(level + 1, hp, duns);
-        path[level] = 0;
-        used[i] = 0;
+        visited[i] = 1;
+        dfs(level + 1, hp - dungeons[i][1], dungeons);
+        visited[i] = 0;
     }
+    
 }
 
+
 int solution(int k, vector<vector<int>> dungeons) {
-    dunNum = dungeons.size();
+    
+    n = dungeons.size();
     
     dfs(0, k, dungeons);
     
