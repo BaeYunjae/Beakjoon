15e0c0548cd04c06392cad404d06edfcd66be237
BaeYunjae <88019800+BaeYunjae@users.noreply.github.com>
2024-05-13 23:31:34 +0900

    [Silver II] Title: 촌수계산, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.cc
+++ 백준/Silver/2644. 촌수계산/촌수계산.cc
@@ -1,22 +1,8 @@
-/* 
-\ 1 2 3 4 5 6 7 8 9
-1 0 1 1 0 0 0 0 0 0
-2 1 0 0 0 0 0 1 1 1
-3 1 0 0 0 0 0 0 0 0
-4 0 0 0 0 1 1 0 0 0
-5 0 0 0 1 0 0 0 0 0
-6 0 0 0 1 0 0 0 0 0
-7 0 1 0 0 0 0 0 0 0 
-8 0 1 0 0 0 0 0 0 0 
-9 0 1 0 0 0 0 0 0 0 
-*/
-
 #include <iostream>
 using namespace std;
 
 int n, a, b, m;
 int family[101][101];
-int parent[101];
 bool visited[101];
 
 int ans = -1;
@@ -43,10 +29,6 @@ int main() {
 	cout.tie(0);
 
 	cin >> n >> a >> b >> m;
-
-	for (int i = 1; i <= n; i++) {
-		parent[i] = i;
-	}
 
 	while (m--) {
 		int x, y;
