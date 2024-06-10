3ef7eed8926aa1221ba46d079c22541dc932b17e
BaeYunjae <88019800+BaeYunjae@users.noreply.github.com>
2024-06-10 23:05:09 +0900

    [Gold V] Title: 탑, Time: 88 ms, Memory: 6300 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.cc
+++ 백준/Gold/2493. 탑/탑.cc
@@ -11,19 +11,16 @@ int main() {
 
 	cin >> n;
 
-	// 스택의 첫번째는 초기값
-	tower.push({ 100000001, 0 });
-
 	for (int i = 1; i <= n; i++) {
 		int height;
 		cin >> height;
 
 		// 스택 마지막의 높이보다 현재 탑의 높이가 작으면 pop
-		while (tower.top().first < height)
+		while (!tower.empty() && tower.top().first < height)
 			tower.pop();
 
-		// 스택 마지막의 번호 출력
-		cout << tower.top().second << " ";
+		if (tower.empty()) cout << 0 << " ";
+		else cout << tower.top().second << " ";
 
 		// 현재 탑의 높이, 현재 탑의 번호 push
 		tower.push({ height, i });
