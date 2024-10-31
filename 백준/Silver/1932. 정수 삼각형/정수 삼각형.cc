67f07a394f1e43ed12ab7274a266ec8aac42c4af
BaeYunjae <88019800+BaeYunjae@users.noreply.github.com>
2024-11-01 00:53:03 +0900

    [Silver I] Title: 정수 삼각형, Time: 8 ms, Memory: 3000 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -2,8 +2,7 @@
 using namespace std;
 
 int n;
-int tri[501][501];
-long long dp[501][501];
+int dp[501][501];
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -13,19 +12,14 @@ int main() {
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j <= i; j++) {
-			cin >> tri[i][j];
+			cin >> dp[i][j];
 		}
-	}
-
-	// 가장 마지막 줄은 그대로 메모
-	for (int i = 0; i < n; i++) {
-		dp[n - 1][i] = tri[n - 1][i];
 	}
 
 	// 마지막에서 바로 윗줄부터 dp에 저장
 	for (int i = n - 2; i >= 0; i--) {
-		for (int j = 0; j < n; j++) {
-			dp[i][j] = max(dp[i + 1][j], dp[i + 1][j + 1]) + tri[i][j];
+		for (int j = n - 2; j >= 0; j--) {
+			dp[i][j] = max(dp[i + 1][j], dp[i + 1][j + 1]) + dp[i][j];
 		}
 	}
 
